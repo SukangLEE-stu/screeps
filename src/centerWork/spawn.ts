aa276c62149c091f3adedfc31e87dd30c444dcaf
gaoxuan <68490371+SukangLEE-stu@users.noreply.github.com>
2022-01-07 23:07:04 +0800

    Add files via upload

---
+++ src/centerWork/spawn.ts
@@ -0,0 +1,24 @@
+
+export function spawnWork(spawn:StructureSpawn){
+    if(spawn.spawning){
+        return;
+    }
+    let tasks = spawn.room.memory.taskCenter.spawnTasks;
+    for(let i = 0;i<tasks.length;++i){
+        if(spawn.store[RESOURCE_ENERGY] >= tasks[i].cost){
+            spawn.spawnCreep(tasks[i].body,tasks[i].name,{
+                memory:tasks[i].creepMemory
+            })
+            spawn.room.memory.taskCenter.spawnTasks.splice(i,1);
+            return;
+        }
+    }
+}
+
+export function calculate(body:BodyPartConstant[]):number{
+    let sum = 0;
+    for(let i = 0;i<body.length;++i){
+        sum += BODYPART_COST[body[i]];
+    }
+    return sum;
+}
---
+++ src/fresh/roomMemory.ts
@@ -0,0 +1,124 @@
+import { calculate } from "centerWork/spawn";
+import { spawn } from "child_process";
+import { initial } from "lodash";
+
+const constCreepNumber:needCreepType = {
+    drone:2,
+    worker:3,
+    transport:3
+  }
+
+export function roomFresh(room:Room){
+    let structures : Structure[] = room.find(FIND_MY_STRUCTURES);
+
+    let mem : centersMemory = {
+            spawns:[]
+
+    }
+
+    for(let i = 0;i<structures.length;++i){
+        switch(structures[i].structureType){
+            case STRUCTURE_SPAWN:
+                mem.spawns.push(structures[i].id);
+                break;
+        }
+    }
+    room.memory.centers = mem;
+}
+
+export function initRoomMemory(room:Room){
+    if(!room.memory.initialed){
+        room.memory.initialed = true;
+
+        if(room.controller && room.controller.my){
+            room.memory.neededCreeps =constCreepNumber;
+
+            let tasks : spawnTask[] = initialCreepTask(room);;
+
+            roomFresh(room);
+
+        }
+        let mine = room.find(FIND_MINERALS);
+        let rareMine = room.find(FIND_DEPOSITS);
+        for(let i = 0;i<mine.length;++i){
+            let source:SourceEnergyMemory = {
+                pos:mine[i].pos,
+                amount:mine[i].density,
+                hasContainer:false,
+                hasContainerFlag:false
+            }
+            room.memory.sources.energy.push(source);
+        }
+        if(rareMine.length){
+            let rareSource : RareEnergyMemory = {
+                pos:rareMine[0].pos,
+                type:rareMine[0].depositType,
+                hasContainerFlag:false,
+                hasContainer:false
+            }
+            room.memory.sources.rareSource = rareSource;
+        }
+    }
+}
+
+function initialCreepTask(room:Room):spawnTask[]{
+    let need:needCreepType = room.memory.neededCreeps;
+    let tasks:spawnTask[] = [];
+
+    let droneTask:spawnTask = {
+        name: "drone",
+        body:[MOVE,MOVE,CARRY,WORK],
+        cost:250,
+        creepMemory:{
+            role:"DRONE",
+            room:room.name,
+            createBeforeDeath:12,
+            working:false
+        }
+    }
+    for(let i = 0;i<need["drone"];++i){
+        let t:spawnTask = droneTask;
+        t.name = t.name+(i-10);
+        tasks.push(t);
+    }
+
+
+    let carryTask:spawnTask = {
+        name: "transport",
+        body:[MOVE,MOVE,CARRY,CARRY],
+        cost:200,
+        creepMemory:{
+            role:"TRANSPORT",
+            room:room.name,
+            createBeforeDeath:12,
+            working:false
+        }
+    }
+
+    for(let i = 0;i<need["transport"];++i){
+        let t:spawnTask = carryTask;
+        t.name = t.name+(i-10);
+        tasks.push(t);
+    }
+
+    let workTask:spawnTask = {
+        name: "work",
+        body:[MOVE,MOVE,CARRY,CARRY,WORK],
+        cost:300,
+        creepMemory:{
+            role:"WORK",
+            room:room.name,
+            createBeforeDeath:15,
+            working:false
+        }
+    }
+
+    for(let i = 0;i<need["worker"];++i){
+        let t:spawnTask = workTask;
+        t.name = t.name+(i-10);
+        tasks.push(t);
+    }
+
+    return tasks;
+
+}
